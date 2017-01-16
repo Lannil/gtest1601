70047b096a38c9f8cc2c69009749d6930adef565
Anni Lajunen <a-lajunen@hotmail.com>
2017-01-16 19:22:04 +0200

    projekti

---
+++ gtest1601/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace FirstTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // määritellään muuttuja
+            string nimi = "Matti Mainio";
+            string nimi2 = "Matti Mainio00000";
+            // tulostetaan muuttujan arvo konsolille
+            Console.WriteLine("Nimi on " + nimi);
+            Console.WriteLine("Nimi on " + nimi2);
+            // pidetään sovelluksen ajoikkuna näytöllä. Sulkeutuu, kun enter-painiketta on painettu
+            Console.ReadLine();
+        }
+    }
+}
